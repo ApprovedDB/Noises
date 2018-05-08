c509488d2dda4f3d5fd0b171ea3b0be4e583fccd
Unknown <approveddb@gmail.com>
2018-05-08 12:57:13 -0400

    Noise Function and Vector2

---
+++ Noises/Math/Vector2.cs
@@ -0,0 +1,108 @@
+﻿namespace Noises.Math
+{
+    public struct Vector2
+    {
+        private double _x;
+        private double _y;
+
+        public double GetX()         => _x;
+        public double SetX(double x) => _x = x;
+        
+        public double GetY()         => _y;
+        public double SetY(double y) => _y = y;
+        
+        public Vector2(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public Vector2(double point)
+        {
+            _x = point;
+            _y = point;
+        }
+
+        public Vector2(float x, float y)
+        {
+            _x = x;
+            _y = y;
+        }
+        
+        public Vector2(float point)
+        {
+            _x = point;
+            _y = point;
+        }
+
+        public void Set(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public void Set(double point)
+        {
+            _x = point;
+            _y = point;
+        }
+
+        public void Set(float x, float y)
+        {
+            _x = x;
+            _y = y;
+        }
+        
+        public void Set(float point)
+        {
+            _x = point;
+            _y = point;
+        }
+
+        public double Dot(Vector2 other)                 => _x * other._x + _y * other._y;
+        public static double Dot(Vector2 a, Vector2 b)   => a._x * b._x + a._y * b._y;
+        
+        public static Vector2 operator +(Vector2 a, Vector2 b)    => new Vector2(a._x + b._x, a._y + b._y);
+        public static Vector2 operator +(Vector2 a, double b)     => new Vector2(a._x + b, a._y + b);
+        public static Vector2 operator +(Vector2 a, float b)      => new Vector2(a._x + b, a._y + b);
+        public static Vector2 operator +(double a, Vector2 b)     => new Vector2(a + b._x, a + b._y);
+        public static Vector2 operator +(float a,  Vector2 b)     => new Vector2(a + b._x, a + b._y);
+        
+        public static Vector2 operator -(Vector2 a, Vector2 b)    => new Vector2(a._x - b._x, a._y - b._y);
+        public static Vector2 operator -(Vector2 a, double b)     => new Vector2(a._x - b, a._y - b);
+        public static Vector2 operator -(Vector2 a, float b)      => new Vector2(a._x - b, a._y - b);
+        public static Vector2 operator -(double a, Vector2 b)     => new Vector2(a - b._x, a - b._y);
+        public static Vector2 operator -(float a, Vector2 b)      => new Vector2(a - b._x, a - b._y);
+
+        public static Vector2 operator *(Vector2 a, Vector2 b)    => new Vector2(a._x * b._x, a._y * b._y);
+        public static Vector2 operator *(Vector2 a, double b)     => new Vector2(a._x * b, a._y * b);
+        public static Vector2 operator *(Vector2 a, float b)      => new Vector2(a._x * b, a._y * b);
+        public static Vector2 operator *(double a, Vector2 b)     => new Vector2(a * b._x, a * b._y);
+        public static Vector2 operator *(float a, Vector2 b)      => new Vector2(a * b._x, a * b._y);
+
+        public static Vector2 operator /(Vector2 a, Vector2 b)    => new Vector2(a._x / b._x, a._y / b._y);
+        public static Vector2 operator /(Vector2 a, double b)     => new Vector2(a._x / b, a._y / b);
+        public static Vector2 operator /(Vector2 a, float b)      => new Vector2(a._x / b, a._y / b);
+        public static Vector2 operator /(double a, Vector2 b)     => new Vector2(a / b._x, a / b._y);
+        public static Vector2 operator /(float a, Vector2 b)      => new Vector2(a / b._x, a / b._y);
+
+
+        public bool Equals(Vector2 other) => _x.Equals(other._x) && _y.Equals(other._y);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj.GetType() == GetType() && Equals((Vector2) obj);
+        }
+        
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = _x.GetHashCode();
+                hashCode = (hashCode * 397) ^ _y.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
---
+++ Noises/Noise/Noise.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Noises.Math;
+using MathD = System.Math;
+
+namespace Noises.Noise
+{
+    public static class Noise
+    {
+        public static double GetFract(double num) => num >= 0 ? num - MathD.Floor(num) : MathD.Ceiling(num);
+        public static float GetFract(float num) => num >= 0 ? num - MathF.Floor(num) : MathF.Ceiling(num);
+
+        public static double GetMixLerp(double x, double y, double value) => x * (1 - value) + y * value;
+        public static float GetMixLerp(float x, float y, float value) => x * (1 - value) + y * value;
+
+        public static double GetRandomDouble(Vector2 vec) => GetFract(MathD.Sin(vec.Dot(new Vector2(12.9898,78.233)) * 43758.5453123));
+        public static double GetRandomDouble(double x, double y) => GetFract(MathD.Sin(new Vector2(x, y).Dot(new Vector2(12.9898,78.233)) * 43758.5453123));
+
+        public static float GetNoise(float x, float y)
+        {
+            return (float)GetNoise((double)x, (double)y);
+        }
+        
+        public static double GetNoise(double x, double y)
+        {
+            Vector2 i = new Vector2(MathD.Floor(x), MathD.Floor(y));
+            Vector2 f = new Vector2(GetFract(x), GetFract(y));
+
+            double a = GetRandomDouble(i);
+            double b = GetRandomDouble(i + new Vector2(1, 0));
+            double c = GetRandomDouble(i + new Vector2(0, 1));
+            double d = GetRandomDouble(i + new Vector2(1, 1));
+
+            Vector2 u = f * f * (3d - 2d * f);
+
+            return GetMixLerp(a, b, u.GetX()) + (c - a) * u.GetY() * (1d - u.GetX()) + (d - b) * u.GetX() * u.GetY();
+        }
+    }
+}
